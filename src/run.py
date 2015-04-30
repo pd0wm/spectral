4d4d7dc0282431d42d6e4c65344e149c609c1d05
Kees Kroep <k.kroep1@gmail.com>
2015-04-30 17:35:28 +0200

    rename run.py

--- src/run.py
+++ src/run.py
@@ -10,12 +10,12 @@ snr = 20
 
 
 # sig = cogradio.source.Sinusoidal(frequencies, SNR=snr)
-sig = cogradio.source.Rect(frequencies, widths)
-samps = sig.generate(samp_freq, window)
+signal = cogradio.source.Rect(frequencies, widths)
+samples = signal.generate(samp_freq, window)
 # plt.plot(samps)
 # plt.show()
 # plt.cla()
-signal_fft = cogradio.fft(samps, samp_freq, window)
+signal_fft = cogradio.fft(samples, samp_freq, window)
 # plt.plot(signal_fft[0], signal_fft[1])
 # plt.show()
 # autocor = cogradio.auto_correlation(samps)
