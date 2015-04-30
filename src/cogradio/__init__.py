d2f2b0ab99fe172af61bed495747dcf040d3e3b8
Dorus Leliveld <dorusleliveld@gmail.com>
2015-04-30 12:34:08 +0200

    Added ipynb notebook for plotting and information read out

--- src/cogradio/__init__.py
+++ src/cogradio/__init__.py
@@ -3,9 +3,6 @@ from . import detection
 from . import source
 from . import reconstruction
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 def signal_power(signal):
     return np.linalg.norm(signal)**2 / len(signal)
 
@@ -13,10 +10,13 @@ def plot(signal):
     plt.plot(signal)
     plt.show()
 
-def plot_fft(signal, samp_freq, window):
+def floor_int_array(array):
+    return int(np.floor(np.min(array)))
+
+def ceil_int_array(array):
+    return int(np.ceil(np.max(array)))
+
+def fft(signal, samp_freq, window):
     fft = np.fft.fftshift(np.abs(np.fft.fft(signal)))
     freq = np.linspace(-samp_freq/2, samp_freq/2, samp_freq*window)
-    plt.plot(freq, fft)
-    plt.xlabel("f")
-    plt.ylabel("|x|")
-    plt.show()
+    return (freq, fft)
