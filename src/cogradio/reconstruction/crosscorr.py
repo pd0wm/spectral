28626c793553e13ca4a93277771c9786d6deccfc
Kees Kroep <k.kroep1@gmail.com>
2015-05-01 16:54:11 +0200

    work on reconstruction py

--- src/cogradio/reconstruction/crosscorr.py
+++ src/cogradio/reconstruction/crosscorr.py
@@ -1,6 +1,6 @@
 from .reconstructor import Reconstructor
 import numpy as np
-
+from scipy.linalg import toeplitz
 
 class CrossCorrelation(Reconstructor):
 
@@ -12,20 +12,32 @@ class CrossCorrelation(Reconstructor):
         self.M = M
         self.C = C
         self.chunk_size = chunk_size
+        self.Rc_Pinv = np.linalg.pinv(self.cross_correlation_filters())
 
     def reconstruct(self, signal):
-        pass
+    	Rx = []
+    	Rx = self.Rc_Pinv*self.cross_correlation_signals(signal)
+
 
     def cross_correlation_signals(self, signal):
-        R = np.zeros((self.M**2, 2*self.chunk_size-1))
+        Ry = np.zeros((self.M**2, 2*self.chunk_size-1))
         for i in range(self.M):
             for j in range(self.M):
-                R[i * self.M + j] = np.correlate(signal[i, :],
+                Ry[i * self.M + j] = np.correlate(signal[i, :],
                                                  signal[j, :],
                                                  mode='full')
-        return R
+        return Ry
 
     def cross_correlation_filters(self):
-        R = np.zeros((self.M**2, self.N, 2))
-        
-        return R
+        Rc0 = Rc1 = np.zeros((self.M**2, self.N))
+        Rc = np.zeros(((2*self.chunk_size-1)*self.M**2, self.N*(2*self.chunk_size-1)))
+        R_tmp = []
+        for i in range(self.M):
+            for j in range(self.M):
+                R_tmp = np.correlate(self.C[i, :],
+                                     self.C[j, :],
+                                     mode='full')
+                Rc0[:,i*self.M+j] = R_tmp[0:self.N-1]
+                Rc1[i*self.M+j] = np.append(np.array([0]), R_tmp[self.N:])#[::-1])
+        Rc = toeplitz(Rc0, Rc1)        
+        return Rc
