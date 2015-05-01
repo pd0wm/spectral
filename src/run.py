2612685ec67afff92655d30bfc0db50de5bf723d
Kees Kroep <k.kroep1@gmail.com>
2015-05-01 17:08:00 +0200

    run.py

--- src/run.py
+++ src/run.py
@@ -5,12 +5,17 @@ import numpy as np
 
 frequencies = [6e3, 20e3]
 widths = [1e3, 5e3]
-samp_freq = 100e3
+samp_freq = 5e2
 window = 0.1
 snr = 20
 N = 5
 M = 2
 C = np.zeros((M, N))
 
-a = cg.reconstruction.CrossCorrelation(N, M, C)
-a.reconstruct([50, 2])
+L = int(samp_freq*window/N)
+print L
+
+signal = np.random.rand(M, L)
+
+a = cg.reconstruction.CrossCorrelation(N, M, C, samp_freq, L)
+a.reconstruct(signal)
