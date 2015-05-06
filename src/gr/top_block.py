9374ad48a29f07252826ff79861fc130690f77ce
Robin Hes <ninjatuna@outlook.com>
2015-05-06 23:21:29 +0200

    Fixed length mismatch/hardcodes, fixed PSD plot axes

--- src/gr/top_block.py
+++ src/gr/top_block.py
@@ -1,19 +1,9 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 ##################################################
-# GNU Radio Python Flow Graph
+# Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Wed May  6 17:58:09 2015
+# Generated: Wed May  6 23:20:49 2015
 ##################################################
-
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
 
 from gnuradio import analog
 from gnuradio import blocks
@@ -34,13 +24,16 @@ class top_block(grc_wxgui.top_block_gui):
 
     def __init__(self):
         grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 32e3
-        self.length = length = 700
+        self.length = length = 200
         self.cos_freq = cos_freq = samp_rate/4
+        self.N = N = 14
 
         ##################################################
         # Blocks
@@ -84,9 +77,9 @@ class top_block(grc_wxgui.top_block_gui):
         	peak_hold=False,
         )
         self.Add(self.wxgui_fftsink2_0.win)
-        self.cogradio_mc_sampling_0 = cogradio.mc_sampling(length, 6, 14)
-        self.cogradio_mc_crosscorr_0 = cogradio.mc_crosscorr(length, 6, 14)
-        self.cogradio_PSD_0 = cogradio.PSD(1386, 10)
+        self.cogradio_mc_sampling_0 = cogradio.mc_sampling(length, N)
+        self.cogradio_mc_crosscorr_0 = cogradio.mc_crosscorr(length, N)
+        self.cogradio_PSD_0 = cogradio.PSD(samp_rate, int(N * (2 * int(length / N) -1)), 10)
         self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, length)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, cos_freq, 1, 0)
@@ -95,13 +88,14 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_add_xx_0, 0))    
-        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_add_xx_0, 1))    
-        self.connect((self.blocks_add_xx_0, 0), (self.blocks_stream_to_vector_0, 0))    
-        self.connect((self.blocks_add_xx_0, 0), (self.wxgui_fftsink2_0, 0))    
-        self.connect((self.blocks_stream_to_vector_0, 0), (self.cogradio_mc_sampling_0, 0))    
-        self.connect((self.cogradio_mc_crosscorr_0, 0), (self.cogradio_PSD_0, 0))    
-        self.connect((self.cogradio_mc_sampling_0, 0), (self.cogradio_mc_crosscorr_0, 0))    
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.blocks_stream_to_vector_0, 0), (self.cogradio_mc_sampling_0, 0))
+        self.connect((self.cogradio_mc_crosscorr_0, 0), (self.cogradio_PSD_0, 0))
+        self.connect((self.cogradio_mc_sampling_0, 0), (self.cogradio_mc_crosscorr_0, 0))
+
 
 
     def get_samp_rate(self):
@@ -129,8 +123,21 @@ class top_block(grc_wxgui.top_block_gui):
         self._cos_freq_text_box.set_value(self.cos_freq)
         self.analog_sig_source_x_1.set_frequency(self.cos_freq)
 
+    def get_N(self):
+        return self.N
+
+    def set_N(self, N):
+        self.N = N
 
 if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     (options, args) = parser.parse_args()
     tb = top_block()
