f1cfea265841ae0e6025850096077318bdfceeec
Robin Hes <ninjatuna@outlook.com>
2015-05-13 17:30:50 +0200

    Cleanup

--- src/cogradio_web/static/js/highcharts_test.js
+++ src/cogradio_web/static/js/highcharts_test.js
@@ -1,10 +1,11 @@
 var socket;
 var output;
+var hostname;
 var toggleButton;
-var samplerate = 32000;
 var chart;
 
 $(document).ready(function() {
+    hostname = document.getElementById("hostname");
     output = document.getElementById("output");
     toggleButton = document.getElementById("toggleButton");
 });
@@ -21,9 +22,12 @@ function onClose (event) {
 
 function onMessage (event) {
     if (typeof event.data == 'string' || event.data instanceof String) {
-        writeLine('Received message: ' + event.data);
+        var data = JSON.parse(event.data);
         chart = $('#container').highcharts();
-        chart.series[0].setData(JSON.parse(event.data));
+        chart.series[0].setData(data.data);
+        socket.send(1);
+
+        fixAxes(data);
     }
     else{
         writeLine('Received binary message');
@@ -44,7 +48,7 @@ function toggleConnect() {
 }
 
 function connect() {
-    socket = new WebSocket("ws://127.0.0.1:9000");
+    socket = new WebSocket("ws://" + hostname.value + ":9000");
     socket.onopen = function(event) { onOpen(event) };
     socket.onclose = function(event) { onClose(event) };
     socket.onmessage = function(event) { onMessage(event) };
@@ -64,20 +68,19 @@ window.addEventListener("load", "init()", false);
 $(function () {
     $('#container').highcharts({
         chart: {
-            zoomType: 'x'
+            zoomType: 'x',
+            animation: false,
         },
         title: {
             text: 'FFT test plot'
         },
         xAxis: {
             type: 'linear',
-            // minRange: -samplerate / 2,
-            // maxRange: samplerate / 2
+            title: { text: 'Frequency [Hz]' }
         },
         yAxis: {
-            title: {
-                text: ''
-            }
+            max: 1000,
+            min: 0
         },
         legend: {
             enabled: false
@@ -92,26 +95,35 @@ $(function () {
                     ]
                 },
                 marker: {
-                    radius: 2
+                    enabled: false
                 },
                 lineWidth: 1,
-                states: {
-                    hover: {
-                        lineWidth: 1
-                    }
-                },
-                threshold: null
+                threshold: null,
+                enableMouseTracking: false
             }
         },
         series: [{
             type: 'area',
             name: 'FFT',
-            pointInterval: 1,
-            // pointStart: -samplerate / 2,
-            // pointEnd: samplerate / 2,
-            data: [
-                0.8446, 0.8445, 0.8444, 0.8451, 0.8418, 0.8264, 0.8258, 0.8232, 0.8233, 0.8258
-            ]
-        }]
+        }],
+        tooltip: {
+            enabled: false
+        }
     });
 });
+
+function fixAxes(data) {
+    var interval = data.sample_freq / data.data.length;
+
+    if (chart.series[0].pointInterval != interval) {
+        chart.series[0].update({
+            pointInterval: interval,
+            pointStart: -data.sample_freq / 2
+        });
+    }
+
+    var ymax = Math.max.apply(Math, data.data);
+    if (chart.yAxis[0].max < ymax) {
+        chart.yAxis[0].update({max: ymax});
+    }
+}
