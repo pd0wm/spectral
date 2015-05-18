ef8106697da58e53137445f0d9b01fc45bfa3f58
Tomwi <tom.aandewiel@gmail.com>
2015-05-18 13:39:47 +0200

    adjust run.py

--- src/run.py
+++ src/run.py
@@ -21,45 +21,26 @@ threshold = 2000
 # Init blocks
 source = cg.source.ComplexExponential(frequencies, SNR=0)
 sampler = cg.sampling.MultiCoset(N)
-C = sampler.generateC()
-#reconstructor2 = cg.reconstruction.CrossCorrelation(N, L, C)
-reconstructor2 = cg.reconstruction.Wessel(N,L)
-
+reconstructor = cg.reconstruction.Wessel(N,L)
 detector = cg.detection.SPFL();
 
-#reconstructor2.reconstruct(None)
-# Compressive sensing
-nyq_signal = source.generate(f_samp, window)
-fp  = open("/home/twiel/ofdm.dmp")
-fp.seek(0, os.SEEK_END)
-size = fp.tell()
-
-fp.seek(1024*1024*10)
-
-plt.ion()
 plt.figure(1)
 
 ax = plt.subplot(121)
 ax2 = plt.subplot(122)
 
-while((size-fp.tell())>window*8):
-    ax.cla()
-    ax2.cla()
-    nyq_signal = sp.fromfile(fp, dtype=sp.complex64, count=window)
-    mc_signal = sampler.sample(nyq_signal)
-    rx = reconstructor2.reconstruct(mc_signal)
+nyq_signal = source.generate(f_samp, window)
+mc_signal = sampler.sample(nyq_signal)
+rx = reconstructor.reconstruct(mc_signal)
 
-    y_s = cg.fft(rx)
+y_s = cg.fft(rx)
 
-    # Axis generation
-    rx_len = (rx.shape[0])
-    detected = detector.detect(rx, len(rx)/numbins, 4E-8)
+# Axis generation
+rx_len = (rx.shape[0])
+detected = detector.detect(rx, len(rx)/numbins, 4E-8)
 
-    f_axis_recon = np.linspace(-0.5, 0.5, rx_len)
+f_axis_recon = np.linspace(-0.5, 0.5, rx_len)
 
-    ax.plot(detected)
-  #  plt.plot(detected)
-    ax2.plot(f_axis_recon, y_s)
-    plt.draw()
-    plt.ylim([0,0.10])
-    sleep(0.02)
+ax.plot(cg.psd(nyq_signal))
+ax2.plot(f_axis_recon, y_s)
+plt.show()
