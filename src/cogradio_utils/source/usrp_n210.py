f5b2c04727cc0af8fc772b17a8cafbcee6ca18a4
Robin Hes <ninjatuna@outlook.com>
2015-05-19 12:41:09 +0200

    Added high-pass filter to shrink LO center peak

--- src/cogradio_utils/source/usrp_n210.py
+++ src/cogradio_utils/source/usrp_n210.py
@@ -19,10 +19,13 @@ class UsrpN210(object):
         self.uhd.set_bandwidth(self.samp_freq / 2, 0)
         self.uhd.set_center_freq(center_freq, 0)
         self.window = [0]
+        self.filter = signal.firwin(25, 200e3, pass_zero=False, nyq=200e3*2)
 
     def generate(self, num_samples):
         if len(self.window) != num_samples:
             self.window = signal.blackmanharris(num_samples)
 
         orig_signal = self.uhd.finite_acquisition(num_samples)
-        return orig_signal * self.window
+
+        return signal.lfilter(self.filter, 1.0, orig_signal * self.window)
+        # return orig_signal * self.window
