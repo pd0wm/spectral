9bfa57c0871799d5f6cec7916329271946d8573c
Willem Melching <willem.melching@gmail.com>
2015-05-20 16:18:06 +0200

    Refactored flaskr.py

--- src/cogradio_web/flaskr.py
+++ src/cogradio_web/flaskr.py
@@ -1,11 +1,9 @@
-from flask import Flask, request, session, g, redirect, url_for, \
-    abort, render_template, flash, jsonify
+from flask import Flask, request, render_template, jsonify
 from flask.ext.bower import Bower
 from element import TextElement, SliderElement, CheckBoxElement
 from content import Content
-import random
 import Pyro4
-import socket
+import logging
 
 settings = Pyro4.Proxy("PYRONAME:cg.settings")
 
@@ -13,7 +11,6 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.debug = True
 
-import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
@@ -23,20 +20,20 @@ gain_slider = SliderElement(key="antenna_gain", title="Antenna gain", value=10, 
 freq_slider = SliderElement(key="center_freq", title="Center Frequency", value=2400, width=3, range=(2.4e3, 2.5e3))
 
 cnt = Content()
-cnt.add(gain_slider, (0, 0))
-cnt.add(freq_slider, (0, 1))
+cnt.add(gain_slider, position=(0, 0))
+cnt.add(freq_slider, position=(0, 1))
 
 
 @app.route('/')
 def index():
-    content = "test"
     return render_template('index.html', content=cnt.html, js_init=cnt.js_init)
 
 
 @app.route('/status')
 def status():
-    sts = cnt.update_eval
-    return jsonify(**sts)
+    # Generate code to run on the client to update elements
+    update_code = cnt.update_eval
+    return jsonify(**update_code)
 
 
 @app.route('/update', methods=["POST"])
@@ -49,7 +46,7 @@ def update():
     cnt.set_by_uuid(uuid, value)
     settings.update(cnt.values)
 
-    return jsonify({uuid: value})
+    return ('', 204)            # Return empty response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
