3d8e804d1cdc4291b8a4185f076db54dd74523a7
Willem Melching <willem.melching@gmail.com>
2015-05-21 14:19:07 +0200

    Removed underscores for private variables

--- src/cogradio_utils/source/filesource.py
+++ src/cogradio_utils/source/filesource.py
@@ -1,20 +1,21 @@
 import numpy as np
 import os
 
+
 class File(object):
 
     def __init__(self, filename, offset=0, dtype=np.complex64):
-        self._dtype = dtype
-        self._itemsize = np.dtype(dtype).itemsize
-        self._file = open(filename, 'rb')
-        self._offset = offset
+        self.data_type = dtype
+        self.item_size = np.dtype(dtype).itemsize
+        self.data_file = open(filename, 'rb')
+        self.offset = offset
 
         # Calculate file length
-        self._file.seek(0, os.SEEK_END)
-        self._length = self._file.tell()
+        self.data_file.seek(0, os.SEEK_END)
+        self.length = self.data_file.tell()
 
     def generate(self, no_samples):
-        self._file.seek((self._offset * self._itemsize) % self._length)
-        samples = np.fromfile(self._file, self._dtype, count=no_samples)
-        self._offset += no_samples
+        self.data_file.seek((self.offset * self.item_size) % self.length)
+        samples = np.fromfile(self.data_file, self.data_type, count=no_samples)
+        self.offset += no_samples
         return samples
