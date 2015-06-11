1477469a00eaf5d5da0c8ff5e8b81addd1a8bb28
Robin Hes <ninjatuna@outlook.com>
2015-06-11 15:45:50 +0200

    More thesis stuff

--- src/generate_thesis_profiling.py
+++ src/generate_thesis_profiling.py
@@ -6,6 +6,7 @@ from tabulate import tabulate
 
 search_items = ['cogradio', 'dist-packages', 'python2.7']
 
+
 def read_stats(inp, key):
     text_stream = StringIO.StringIO()
     stats = pstats.Stats(inp, stream=text_stream)
@@ -14,12 +15,14 @@ def read_stats(inp, key):
     text_stream.seek(0)
     return text_stream
 
+
 def find_highest_index(lst, item):
-    indices = [i for i,x in enumerate(lst) if x == item]
+    indices = [i for i, x in enumerate(lst) if x == item]
     if indices:
         return max(indices)
     else:
         return None
+
 
 def clean_filename_entry(entry):
     entry = entry.split("/")
@@ -28,6 +31,7 @@ def clean_filename_entry(entry):
         if index is not None:
             entry = entry[index:]
     return "/".join(entry)
+
 
 def get_content_from_file(prof_rep):
     for line in prof_rep:
@@ -44,12 +48,14 @@ def get_content_from_file(prof_rep):
 
     return {'header': header, 'data': data}
 
+
 def table_to_file(table, filename):
     build_table = tabulate(content['data'], headers=content['header'], tablefmt="latex_booktabs")
     with open(filename, 'w') as f:
         f.write(build_table)
 
-OUTPUT_DIR = "../doc/thesis/appendix/profiling_reports/"
+OUTPUT_DIR = "../doc/thesis/appendix/profiler_reports/"
+APPENDIX_NAME = "profiler_reports.tex"
 INPUT_DIR = "./profiler_reports/"
 OUTPUT_EXT = ".tex"
 key = ('cumtime', 'tottime')
@@ -62,3 +68,7 @@ for filetupl in inp_files:
         raw_report = read_stats(INPUT_DIR + input_file, key)
         content = get_content_from_file(raw_report)
         table_to_file(content, OUTPUT_DIR + output_file)
+
+lines = ["\\input{{{}}}\n".format(f[0]) for f in inp_files]
+with open(OUTPUT_DIR + APPENDIX_NAME, 'w') as f:
+    f.writelines(lines)
