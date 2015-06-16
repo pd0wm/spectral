490b3cc756bc8ed433b8c3dc393337b6227726b7
Dorus Leliveld <dorusleliveld@gmail.com>
2015-06-16 15:37:39 +0200

    Removed bug

--- src/cogradio/__init__.py
+++ src/cogradio/__init__.py
@@ -2,7 +2,6 @@ from . import sampling
 from . import detection
 from . import source
 from . import reconstruction
-from . import jamming
 from .settings import *
 from .helperfunctions import *
 from .sparseruler import *
