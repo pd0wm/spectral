bef64e720df7e51b8f923176c461982f61a49486
Dorus Leliveld <dorusleliveld@gmail.com>
2015-06-18 21:19:59 +0200

    Restructured samplers to better reflect code reuse and actual working principles

--- src/cogradio/sampling/multicoset.py
+++ src/cogradio/sampling/multicoset.py
@@ -1,5 +1,4 @@
 import numpy as np
-import cogradio as cg
 from .sampling import Sampler
 
 
@@ -7,14 +6,11 @@ class MultiCoset(Sampler):
 
     """Multi coset sampler implementation"""
 
-    def __init__(self, N):
+    def __init__(self, S, N, M):
         super(MultiCoset, self).__init__()
-        self.C = self.generate_C(N)
+        self.C = self.build_sampling_matrix(S, N, M)
         self.N = self.C.shape[1]
-
-    def generate_C(self, N):
-        sparseruler = cg.sparseruler(N)
-        return cg.build_sparse_ruler_sampling_matrix(sparseruler, N)
+        self.M = self.C.shape[0]
 
     def sample(self, x):
         length = x.shape[0]
@@ -24,3 +20,9 @@ class MultiCoset(Sampler):
         x = np.reshape(x.T, (self.N, -1), order='F')
         y = np.dot(self.C, x)
         return y
+
+    def build_sampling_matrix(self, intervals, N, M):
+        C = np.zeros((M, N), np.complex128)
+        for i, j in enumerate(intervals):
+            C[i, j] = 1
+        return C
