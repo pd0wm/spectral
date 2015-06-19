772d3afcbb31887a09173868fbb348307042e6c9
Dorus Leliveld <dorusleliveld@gmail.com>
2015-06-19 19:30:28 +0200

    More documentation

--- src/cogradio/sampling/sampling.py
+++ src/cogradio/sampling/sampling.py
@@ -1,12 +1,22 @@
 class Sampler(object):
 
-    """A parent class for sampling methods"""
+    """Interface for all samplers in this subpackage"""
 
     def __init__(self):
         pass
 
     def sample(self, signal):
+        """ Contract that all children implement sample method.
+
+        Raises:
+            NotImplementedError: If child does not implement.
+        """
         raise NotImplementedError("Implement this method")
 
     def get_C(self):
+        """ Getter function for use in combination with reconstructors and
+        other classes that might need the sample matrix.
+
+        Returns:
+           sampling matrix. """
         return self.C
