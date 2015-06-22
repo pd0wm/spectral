61be22ec3f6f0a3e9e7734a8b108688912d3ddee
Dorus Leliveld <dorusleliveld@gmail.com>
2015-06-22 10:00:20 +0200

    testfile

--- src/non_mp_test.py
+++ src/non_mp_test.py
@@ -1,7 +1,6 @@
 import spectral.core as sc
 import spectral.supervisor as ss
 from multiprocessing import Process
-import processes as pr
 import time
 
 dump_file_path = 'dumps/ofdm.dmp'
@@ -16,12 +15,30 @@ source = sc.source.File(dump_file_path)
 sampler = sc.sampling.MinimalSparseRuler(N)
 reconstructor = sc.reconstruction.Wessel(L, sampler.get_C())
 
-signal_queue = ss.multiprocessing.SafeQueue()
-detection_queue = ss.multiprocessing.SafeQueue()
 
-websocket_src_queue = ss.multiprocessing.SafeQueue()
-websocket_rec_queue = ss.multiprocessing.SafeQueue()
-websocket_det_queue = ss.multiprocessing.SafeQueue()
+def signalgen(source, sampler, queue):
+    while True:
+        inp = source.generate(block_size)
+        sam = sampler.sample(inp)
+        queue.queue(sam)
+
+
+def reconstruct(recon, queue):
+    inp = 0
+    num = 1000
+    t = time.time()
+    while True:
+        sam = queue.dequeue()
+        if inp == num:
+            total = time.time() - t
+            print "mp", total / num
+        if sam is not None:
+            inp += 1
+            out = reconstructor.reconstruct(sam)
+
+
+
+signal_queue = ss.multiprocessing.SafeQueue()
 
 num = 1000
 t = time.time()
@@ -32,10 +49,11 @@ for i in range(num):
 total = time.time() - t
 print "non mp", total / num
 
-p1 = Process(target=pr.run_generator,
-             args=(signal_queue, websocket_src_queue, source, sampler, sample_freq, block_size, upscale_factor))
-p2 = Process(target=pr.run_reconstructor,
-             args=(signal_queue, websocket_rec_queue, detection_queue, reconstructor, sample_freq))
+p1 = Process(target=signalgen,
+             args=(source, sampler, signal_queue,))
+
+p2 = Process(target=reconstruct,
+             args=(reconstructor, signal_queue,))
 proc = [p1, p2]
 [p.start() for p in proc]
 [p.join() for p in proc]
